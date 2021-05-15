a6f27614a70e4f1ba333f2bbd9139329ddbc169f
RuslanTurchaninov <72912270+RuslanTurchaninov@users.noreply.github.com>
2021-05-16 00:14:06 +0600

    Update index.js

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -10,7 +10,6 @@
 /*
  homeworkContainer - это контейнер для всех ваших домашних заданий
  Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер
-
  Пример:
    const newDiv = document.createElement('div');
    homeworkContainer.appendChild(newDiv);
@@ -21,7 +20,18 @@ const homeworkContainer = document.querySelector('#app');
 
 document.addEventListener('mousemove', (e) => {});
 
-export function createDiv() {}
+export function createDiv() {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add('draggable-div');
+  newDiv.style.backgroundColor = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
+  newDiv.style.width = Math.random() + 'px';
+  newDiv.style.height = Math.random() + 'px';
+  newDiv.style.top = Math.random() + 'px';
+  newDiv.style.bottom = Math.random() + 'px';
+  newDiv.style.left = Math.random() + 'px';
+  newDiv.style.right = Math.random() + 'px';
+  return newDiv;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
